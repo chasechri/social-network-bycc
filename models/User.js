ee6438d409d5d7debdc8c47f8614f3ddb27ef869
chasechri <christensonchase451@gmail.com>
2022-08-29 13:56:47 -0500

    models

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require("mongoose");
+// const dateFormat = require("../utils/dateFormat");
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.lenght;
+});
+
+const User = model("User", UserSchema);
+
+module.exports = User;
